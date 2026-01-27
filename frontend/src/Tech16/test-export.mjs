61ce68ca42165980df284013f68a118142fcb333
calebnewtonusc <joelnewto@usc.edu>
2026-01-26 22:41:21 -0800

    Remove temporary test files

--- frontend/src/Tech16/test-export.mjs
+++
@@ -1,7 +0,0 @@
-// Test if generateScoresFromType can be imported
-import { generateScoresFromType } from './scoringSupabase.js';
-
-console.log('✓ generateScoresFromType imported successfully');
-console.log('Testing with type code U-E-V-A:');
-const scores = generateScoresFromType('U-E-V-A');
-console.log(scores);
